e2856efb80b0a7da20c699a6bdbe09506f54385a
Ilies Oana Elena <117008890+oanailies@users.noreply.github.com>
2025-07-16 12:29:31 +0300

    Add files via upload

---
+++ Assigment 1/backend/users/src/main/java/users/controller/UserController.java
@@ -0,0 +1,79 @@
+package users.controller;
+
+import users.dto.LoginRequest;
+import users.entities.User;
+import users.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User newUser = userService.register(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User newUser = userService.createUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping("/roleUser")
+    public List<User> getUsersWithRoleUser() {
+        return userService.getAllUsersWithRoleUser();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User updatedUser = userService.updateUser(id, userDetails);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest loginRequest) {
+        User user = userService.login(loginRequest.getEmail(), loginRequest.getPassword());
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (user != null) {
+            response.put("message", "Login successful!");
+            response.put("user", user);
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("message", "Invalid email or password.");
+            return ResponseEntity.status(401).body(response);
+        }
+    }
+}
